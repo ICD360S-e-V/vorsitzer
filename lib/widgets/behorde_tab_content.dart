7541af43d65a40145f6dd6e74c918d366dea6c0e
icd360sevofficial <github@icd360s.de>
2026-04-10 17:22:08 +0000

    fix: revert saveFile back to FilePicker.platform.saveFile in behorde_tab_content.dart

--- lib/widgets/behorde_tab_content.dart
+++ lib/widgets/behorde_tab_content.dart
@@ -1792,7 +1792,7 @@ class _BehoerdeTabContentState extends State<BehoerdeTabContent> {
                                   try {
                                     final response = await widget.apiService.downloadAntragDokument(doc['id'] as int);
                                     if (response.statusCode == 200) {
-                                      final savePath = await FilePickerHelper.saveFile(
+                                      final savePath = await FilePicker.platform.saveFile(
                                         dialogTitle: 'Dokument speichern',
                                         fileName: doc['filename'] ?? 'dokument',
                                       );
